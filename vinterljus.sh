d2befdc280b516f77889830d1eb1f59ce6254d02
vinterljus <vinterljus@vinterljus-rasptest.(none)>
2013-11-11 00:12:47 +0100

    Buggfix in vinterljus.sh

--- vinterljus.sh
+++ vinterljus.sh
@@ -1,25 +1,25 @@
 #! /bin/bash
 
-sleep(2)
+sleep 2 
 echo "-----------------------"
 echo "Starting Camera .. "
 sudo python /home/pi/pi-vinterljus/campy/campy.py &
-sleep(1)
+sleep 1
 echo "Camera running!"
 echo "-----------------------"
 echo "Starting PIR .."
 sudo python /home/pi/pi-vinterljus/pir/pir.py &
-sleep(1)
+sleep 1
 echo "PIR running!"
 echo "-----------------------"
 echo "Starting PiAlive .."
 sudo python /home/pi/pi-vinterljus/pialive/pialive.py &
-sleep(1)
+sleep 1
 echo "PiAlive running!"
 echo "-----------------------"
 echo "Starting PiButton .."
 sudo python /home/pi/pi-vinterljus/pibutton/pibutton.py &
-sleep(1)
+sleep 1
 echo "PiButton running!"
 echo "-----------------------"
 echo "PI-VINTERLJUS INITIATED"

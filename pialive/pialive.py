a45b77914b7034f7881b6b8867f49ff04a406576
vinterljus <vinterljus@vinterljus-rasptest.(none)>
2013-11-12 14:58:12 +0100

    Removed debugg messages

--- pialive/pialive.py
+++ pialive/pialive.py
@@ -11,14 +11,14 @@ def main():
     HOST = '192.168.10.10'
     PORT = 50666
 
-    alive = "PiAlive "
+    alive = "PiAlive, "
     command = "/opt/vc/bin/vcgencmd measure_temp"
 
     while(True):
         time.sleep(3)
         cpuTemp = (subprocess.check_output(command, shell=True))
         msg = alive + cpuTemp
-        print msg
+        #print msg
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         s.connect((HOST, PORT))
         s.send(msg)

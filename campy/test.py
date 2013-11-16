15d2de2cfe7c91a556bf691de63dd2c879803502
vinterljus <vinterljus@vinterljus-rasptest.(none)>
2013-11-16 01:18:37 +0100

    opencv test

--- campy/test.py
+++ campy/test.py
@@ -5,14 +5,49 @@ import time
 os.system("sudo pkill uv4l")
 os.system("sudo uv4l --driver raspicam --auto-video_nr --extension-presence=1 --encoding rgba --width 320 --height 240 --nopreview")
 
-cam = cv2.VideoCapture(0)
-s, img = cam.read()
-if s:
-    print "Img was read"
-    namedWindow("cam-test",CV_WINDOW_AUTOSIZE)
-    imshow("cam-test", img)
-    waitkey(0)
-    time.sleep(1)
-    destroyWindow("cam-test")
-else:
-    print "Could not get img"
+class CamTest():
+
+
+    threshold = 20
+    sensitivity = 180
+    colorChannel = 1
+    oldImg = 0
+    img = 0
+    s = 1
+    cam = 0
+
+    def __init__(self):
+
+        self.cam = cv2.VideoCapture(0)
+
+    def run(self):
+        while self.s:
+            time.sleep(0.05)
+            self.s, self.img = self.cam.read()
+    
+            if self.oldImg != 0:
+                motionDetect()
+            self.oldImg = self.img
+
+    def motionDetect(self):
+    
+        pixDiff = 0
+        changedPixels = 1
+        for x in range(width):
+            for y in range(height):
+                newPix = self.img[x][y][self.colorChannel]
+                oldPix = self.oldImg[x][y][self.colorChannel]
+                pixDiff = abs(newPix - oldPix)
+            
+                if pixDiff > self.threshold:
+                    changedPixels += 1
+                
+                if changedPixels > self.sensitivity:
+                    print "motion detected"
+                    return
+
+        print "motionless"
+
+
+camTest = CamTest()
+camTest.run()
